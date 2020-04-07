859237cf003d5a52052c0246e390dce67065c80f
piteren <piter@packshot.pl>
2020-04-07 10:51:38 +0200

    GamesManager code update

--- run_games.py
+++ run_games.py
@@ -22,8 +22,9 @@ class GamesManager:
             n_dmk=          14,
             dmk_players=    150):
 
-        tpl_count = 3 # hardcoded
-        assert (n_dmk * dmk_players) % tpl_count == 0
+        self.tpl_count = 3 # hardcoded
+        assert (n_dmk * dmk_players) % self.tpl_count == 0
+        self.tables = [] # list of tables
 
         # create DMKs
         #self.dmkL = [ProDMK(name='dmk%d' % ix, n_players=dmk_players) for ix in range(n_dmk)]
@@ -37,34 +38,41 @@ class GamesManager:
             stats_iv=       10000,
             verb=           1) for ix in range(n_dmk)]
 
-        # get all ques into one dict
+    # creates tables based on ques of DMKs
+    def _create_tables(self):
+
+        # build dict of ques tuples, for all players: (IN,OUT)
         ques = {}
         for dmk in self.dmkL:
             pl_oq = dmk.pl_out_que
             pl_iqD = dmk.pl_in_queD
             for k in pl_iqD:
                 ques[k] = (pl_iqD[k],pl_oq)
-
-        # create tables
         ques_keys = list(ques.keys())
         random.shuffle(ques_keys)
-        self.tables = []
+
+        # create tables
+        tables = []
         table_queD = {}
         for k in ques_keys:
             table_queD[k] = ques[k]
-            if len(table_queD) == tpl_count:
+            if len(table_queD) == self.tpl_count:
                 table = QPTable(
                     pl_ques=    table_queD,
-                    name=       'tbl%d' % len(self.tables),
+                    name=       f'tbl{len(tables)}',
                     # verb=       self.verb,
                 )
-                self.tables.append(table)
+                tables.append(table)
                 table_queD = {}
+        return tables
 
     # runs processed games
     def run_games(self):
+
         print('Starting DMKs...')
         for dmk in tqdm(self.dmkL): dmk.start()
+
+        self.tables = self._create_tables()
         print('Starting tables...')
         for tbl in tqdm(self.tables): tbl.start()
 
